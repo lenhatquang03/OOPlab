5168dc29b5ecd8afd9d18629314e3622a86866b0
lenhatquang03 <buile.quang7804@gmail.com>
2024-05-15 08:04:00 +0700

    UnfinishedBook

---
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -0,0 +1,57 @@
+package AimsProject.src.hust.soict.dsai.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    private List<String> authors = new ArrayList<String>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public static void addAuthor(String authorName) {
+
+    }
+}
