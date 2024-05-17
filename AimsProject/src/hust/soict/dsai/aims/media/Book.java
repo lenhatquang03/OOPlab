9189d1a82d3439f2ca7c2f5d9e7a34ab380d4704
lenhatquang03 <buile.quang7804@gmail.com>
2024-05-17 10:14:37 +0700

    4. Remove fields and methods from Book and DigitalVideoDisc class

--- AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -5,44 +5,7 @@ import java.util.List;
 
 public class Book {
     // Attributes
-    private int id;
-    private String title;
-    private String category;
-    private float cost;
     private List<String> authors = new ArrayList<String>();
-
-    // Accessor methods
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public float getCost() {
-        return cost;
-    }
-
-    public void setCost(float cost) {
-        this.cost = cost;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     // Constructor
     public Book() {}
