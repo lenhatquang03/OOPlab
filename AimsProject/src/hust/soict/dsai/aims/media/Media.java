6cb0a20ebf98320aa1b556fdaf68688be00c99dc
lenhatquang03 <buile.quang7804@gmail.com>
2024-05-17 10:05:26 +0700

    3, 4. Add fields to the Book and create the Media class with its fields

---
+++ AimsProject/src/hust/soict/dsai/aims/media/Media.java
@@ -0,0 +1,42 @@
+package AimsProject.src.hust.soict.dsai.aims.media;
+
+public class Media {
+    // Attributes
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+
+    // Accessor Methods
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+}
