89c76a4b5210fe50b75828fc6e2d5bbd81c12488
lenhatquang03 <buile.quang7804@gmail.com>
2024-05-17 10:59:22 +0700

    UnfinishedBook

---
+++ AimsProject/src/hust/soict/dsai/aims/media/Disc.java
@@ -0,0 +1,25 @@
+package AimsProject.src.hust.soict.dsai.aims.media;
+
+public class Disc extends Media{
+    // Attributes
+    private int length;
+    private String director;
+
+    // Accessor methods
+    public int getLength() {
+        return length;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    // Constructors
+    public Disc() {}
+
+    public Disc(int id, String title, String category, float cost, String director, int length) {
+        super(id, title, category, cost);
+        this.director = director;
+        this.length = length;
+    }
+}
