a2c029be12d35b2a31c72bfe62f00035ae1e776d
lenhatquang03 <buile.quang7804@gmail.com>
2024-05-18 21:25:03 +0700

    Polymorphism with toString() method

--- AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -1,6 +1,10 @@
 package hust.soict.dsai.aims.media;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class CompactDisc extends Disc implements Playable {
     // Attributes
@@ -13,9 +17,10 @@ public class CompactDisc extends Disc implements Playable {
     }
 
     // Constructor
-    public CompactDisc(String title, String category, String director, String artist) {
+    public CompactDisc(String title, String category, String director, String artist, int id) {
         super(title, category, director);
         this.artist = artist;
+        this.setId(id);
     }
 
     // Methods
@@ -54,5 +59,60 @@ public class CompactDisc extends Disc implements Playable {
         for (Track track : tracks) {
             track.play();
         }
+    }
+
+    @Override
+    public String toString() {
+        String name = String.format("%d.", this.getId()) + "CD - ";
+        Class<?> cls = this.getClass();
+        while (cls.getSuperclass() != null) {
+            // Accessing the inherited fields of the class.
+            cls = cls.getSuperclass();
+            Field[] clsFields = cls.getDeclaredFields();
+            for (int i = 0; i < clsFields.length; i++) {
+                // Accessing the simple fieldName like "attribute".
+                String lowerField = clsFields[i].toString().split(String.format("%s.", cls.toString().split(" ")[1]))[1];
+                // Transform "attribute" to "Attribute" so we can invoke the method name "getAttribute".
+                String fieldName = String.valueOf(lowerField.charAt(0)).toUpperCase() + lowerField.substring(1);
+                try {
+                    Method getter = this.getClass().getMethod("get" + fieldName);
+                    Object attribute = getter.invoke(this);
+                    if (!cls.getSuperclass().toString().equals("class java.lang.Object")) {
+                        if (!Objects.isNull(attribute)) {
+                            if (lowerField.equals("cost")) {
+                                name += (fieldName + ":" + (String.format("%s$ - ", attribute.toString())));
+                            } else {
+                                name += (fieldName + ":" + (String.format("%s - ", attribute.toString())));
+                            }
+                        } else {
+                            name += (fieldName + ":null - ");
+                        }
+                    } else {
+                        if (!Objects.isNull(attribute)) {
+                            if (lowerField.equals("cost")) {
+                                if (i == clsFields.length - 1) {
+                                    name += (fieldName + ":" + (String.format("%s$.", attribute.toString())));
+                                } else {
+                                    name += (fieldName + ":" + (String.format("%s$ - ", attribute.toString())));
+                                }
+                            } else {
+                                if (i == clsFields.length - 1) {
+                                    name += (fieldName + ":" + (String.format("%s.", attribute.toString())));
+                                } else {
+                                    name += (fieldName + ":" + (String.format("%s - ", attribute.toString())));
+                                }
+                            }
+                        }
+                    }
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return name;
     }
 }
--- AimsProject/src/hust/soict/dsai/aims/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/aims/test/store/StoreTest.java
@@ -1,6 +1,9 @@
 package hust.soict.dsai.aims.test.store;
 
+import hust.soict.dsai.aims.media.Book;
+import hust.soict.dsai.aims.media.CompactDisc;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.store.Store;
 
 public class StoreTest {
@@ -10,20 +13,26 @@ public class StoreTest {
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99f);
         DigitalVideoDisc dvd4 = new DigitalVideoDisc();
-        DigitalVideoDisc[] aDVDs = {dvd1, dvd2, dvd3};
-        myStore.addMedia(aDVDs);
-        
-        System.out.println("List of DVDs in store after adding some: ");
-        for (int i = 0; i < myStore.getItemsInStore().size(); i++) {
-            System.out.println(myStore.getItemsInStore().get(i).toString());
-        }
+        CompactDisc CD = new CompactDisc("The Blue planet", "Documentary", "David Attenborough", "Artist", 2);
+        Book book = new Book("How do you live?", "Philosophical", 3);
+        Media[] aDVDs = {dvd1, CD, book};
+//        myStore.addMedia(aDVDs);
 
-        System.out.println();
-        DigitalVideoDisc[] rDVDs = {dvd2, dvd4};
-        myStore.removeMedia(rDVDs);
-        System.out.println("List of DVDs in store after removing some: ");
-        for (int i = 0; i < myStore.getItemsInStore().size(); i++) {
-            System.out.println(myStore.getItemsInStore().get(i).toString());
-        }      
+        for (Media media : aDVDs) {
+            System.out.println(media.toString());
+        }
+        
+//        System.out.println("List of DVDs in store after adding some: ");
+//        for (int i = 0; i < myStore.getItemsInStore().size(); i++) {
+//            System.out.println(myStore.getItemsInStore().get(i).toString());
+//        }
+//
+//        System.out.println();
+//        DigitalVideoDisc[] rDVDs = {dvd2, dvd4};
+//        myStore.removeMedia(rDVDs);
+//        System.out.println("List of DVDs in store after removing some: ");
+//        for (int i = 0; i < myStore.getItemsInStore().size(); i++) {
+//            System.out.println(myStore.getItemsInStore().get(i).toString());
+//        }
     } 
 }
